6f3d72375a29b7858389a676a4aa18100bc21916
Shrivastavavineet <vshrivastava902@gmail.com>
2026-06-03 07:44:36 +0000

    Fresh Maven project structure with pom.xml and sample tests

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins Assignment 5!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
